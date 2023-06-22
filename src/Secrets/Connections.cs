39fb9a456633cc43d09a2fc44fcce93fbabb85b1
lailabougria <laila.bougria@particular.net>
2023-06-22 15:59:54 +0200

    sight

--- src/Secrets/Connections.cs
+++ src/Secrets/Connections.cs
@@ -2,5 +2,5 @@
 
 public static class Connections
 {
-    public static string AzureServiceBus = "Endpoint=sb://lb-otel.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RW6AhfAf42M8IzLMYSJRAQVv87+n3ZkPV+ASbJzpha0=";
+    public static string AzureServiceBus = "insert-connection-string";
 }
